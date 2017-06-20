fce2996bb76a69a66c6a886804d49015a458d126
paddymcglone <paddymcglone@gmail.com>
2017-06-20 19:14:04 +0100

    Adding Authorize

---
+++ MVC5Fundamentals/Identity/Controllers/SecretController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace Identity.Controllers
+{
+    [Authorize]
+    public class SecretController : Controller
+    {                
+        public ActionResult Secret()
+        {
+            return Content("This is a secret!");
+        }
+        
+        // Anonymous overrides the authorize attribute on the controller
+        [AllowAnonymous]
+        public ActionResult Overt()
+        {
+            return Content("This is not a secret");
+        }
+    }
+}
