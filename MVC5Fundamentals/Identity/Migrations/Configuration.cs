e0bab0907b36b4e8400b56b952569946c6f8f06a
paddymcglone <paddymcglone@gmail.com>
2017-06-21 19:07:36 +0100

    Seeding users

--- MVC5Fundamentals/Identity/Migrations/Configuration.cs
+++ MVC5Fundamentals/Identity/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 using Identity.Models;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Identity.Migrations
 {
@@ -14,11 +15,18 @@ namespace Identity.Migrations
 
         protected override void Seed(Identity.Models.ApplicationDbContext context)
         {
-            var hash = new PasswordHasher();
-            context.Users.AddOrUpdate(u => u.UserName,
-                new ApplicationUser{UserName = "pmcglone", PasswordHash = hash.HashPassword("Password") });
+            /* The old method of adding users */
+            //var hash = new PasswordHasher();
+            //context.Users.AddOrUpdate(u => u.UserName,
+            //    new ApplicationUser{UserName = "pmcglone", PasswordHash = hash.HashPassword("Password") });
 
-            
+
+            /* The new method of adding users */
+            var store = new UserStore<ApplicationUser>(context);
+            var manager = new UserManager<ApplicationUser>(store);
+            var user = new ApplicationUser {UserName = "pmcglone", Email = "paddy@home.local"};
+
+            manager.Create(user, "password");
         }
     }
 }
