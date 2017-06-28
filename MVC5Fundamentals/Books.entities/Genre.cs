6d3333e16e59f9768bce37488a552677e3a122c5
paddymcglone <paddymcglone@gmail.com>
2017-06-28 18:45:54 +0100

    EntityFramework-Creating simple models

---
+++ MVC5Fundamentals/Books.entities/Genre.cs
@@ -0,0 +1,10 @@
+namespace Books.entities
+{
+    public enum Genre
+    {
+        Fiction = 0,
+        nonFiction = 1,
+        scienceFiction = 2,
+        Factual = 3
+    }
+}
---
+++ MVC5Fundamentals/Books.entities/book.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Books.entities
+{
+    public class book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        public Genre Category { get; set; }
+    }
+}
